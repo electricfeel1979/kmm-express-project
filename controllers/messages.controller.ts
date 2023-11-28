e256fed4c36abd08e2acbee6546ef7bea89ab999
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2023-11-28 20:05:38 +0800

    97. Sending Files - 6min

--- controllers/messages.controller.ts
+++ controllers/messages.controller.ts
@@ -1,7 +1,10 @@
 import {Request, Response} from 'express';
 
+import path from 'path';
+
 function getMessages(req: Request, res: Response) {
-  res.send('<ul><li>Helloo Albert!</li></ul>');
+  //   res.send('<ul><li>Helloo Albert!</li></ul>');
+  res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpg'));
 }
 
 function postMessage() {
